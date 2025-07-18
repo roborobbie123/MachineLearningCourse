843984e6b1de9ad582cef393012c33d9311bca4a
Robert Barrett <roborobbie123@gmail.com>
2025-07-18 09:50:01 -0400

    completed the polynomial regression analysis

--- regression/polynomialRegression/practice.py
+++ regression/polynomialRegression/practice.py
@@ -15,7 +15,29 @@ lin_model.fit(X, y)
 
 # Training Polynomial Regression model on entire dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_model = PolynomialFeatures(degree = 2)
+poly_model = PolynomialFeatures(degree = 4)
 X_poly = poly_model.fit_transform(X)
 lin_model_2 = LinearRegression()
-lin_model_2 = fit(X_poly, y)
+lin_model_2.fit(X_poly, y)
+
+# Visualizing Linear Regression Results
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_model.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualizing Polynomial Regression Results
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_model_2.predict(X_poly), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+# Predicting a new result with linear regression
+print(lin_model.predict([[6.5]]))
+
+# Predicting a new result with polynomial regression
+print(lin_model_2.predict(poly_model.fit_transform([[6.5]])))
