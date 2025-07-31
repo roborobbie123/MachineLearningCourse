dc2101664d6f2cb4fe7ccd0debf9b7dfc1689470
Robert Barrett <roborobbie123@gmail.com>
2025-07-31 18:13:51 -0400

    built the ANN model

--- deepLearning/ANN/practice.py
+++ deepLearning/ANN/practice.py
@@ -18,4 +18,43 @@ from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 
+# Splitting data into training and test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Initialize the ANN Model
+ann = tf.keras.models.Sequential()
+
+# Adding the input layer and the first hidden layer
+ann.add(tf.keras.layers.Dense(units = 6, activation = 'relu'))
+
+# Adding the second hidden layer (same line as above)
+ann.add(tf.keras.layers.Dense(units = 6, activation = 'relu'))
+
+# Adding the output layer
+ann.add(tf.keras.layers.Dense(units = 1, activation = 'sigmoid'))
+
+# Compiling the ANN
+ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Training ANN on training set
+ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
+
+# Making predictions
+print(ann.predict(sc.transform([[1, 0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])) > 0.5)
+
+# Predicting test set results
+y_pred = ann.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+matrix = confusion_matrix(y_test, y_pred)
+score = accuracy_score(y_test, y_pred)
+print(matrix)
+print(score)
